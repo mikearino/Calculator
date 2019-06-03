1a43c5d26537ea6e24578c01fa9b2a94b6fb5e5c
Abdellah Ramadan and Hannah Melendy <hannah@>
2019-06-03 11:50:43 -0700

    add calculator functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,35 @@
 var add = function(number1, number2) {
   return number1 + number2;
-};
+}
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
+alert(result);
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+}
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number"));
+var result = subtract(number1, number2);
+alert(result);
+
+var multiply = funtion(number1, number2) {
+  return number1 * number2;
+}
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number"));
+var results = multiply(number1, number2);
+alert(result);
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+}
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var results = divide(number1, number2);
 alert(result);
