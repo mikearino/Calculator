6031472a64667dabca000a8a5348ef214cf0a5e7
Abdellah Ramadan and Mike Lofgren <mike@>
2019-06-05 15:42:11 -0700

    Split up front end and back end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,47 @@
+/* Back End */
 var add = function(number1, number2) {
   return number1 + number2;
-}
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+};
 
 var subtract = function(number1, number2) {
   return number1 - number2;
-}
+};
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number"));
-var result = subtract(number1, number2);
-alert(result);
-
-var multiply = funtion(number1, number2) {
+var multiply = function(number1, number2) {
   return number1 * number2;
-}
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number"));
-var results = multiply(number1, number2);
-alert(result);
+};
 
 var divide = function(number1, number2) {
   return number1 / number2;
-}
+};
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var results = divide(number1, number2);
-alert(result);
+
+
+
+
+/* Front End */
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+   var number1 = parseInt($("#add1").val());
+   var number2 = parseInt($("#add2").val());
+   var result = add(number1, number2);
+   $("#output").text(result);
+
+
+  // var number1 = parseInt(prompt("Enter a number:"));
+  // var number2 = parseInt(prompt("Enter another number"));
+  // alert(subtract(number1, number2));
+  //
+  //
+  // var number1 = parseInt(prompt("Enter a number:"));
+  // var number2 = parseInt(prompt("Enter another number"));
+  // alert(multiply(number1, number2));
+  //
+  //
+  // var number1 = parseInt(prompt("Enter a number:"));
+  // var number2 = parseInt(prompt("Enter another number:"));
+  // alert(divide(number1, number2));
+
+  });
+});
